cb49a40510fc30b38c06c8665105ab413673ca2e
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-06-20 12:17:11 +0400

    Time: 37 ms (48.15%), Space: 24 MB (81.81%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int right = 1;
+        ans[0] = 1;
+        for(int i = 1; i<nums.size(); i++)
+        {
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+        for(int i = nums.size()-1; i>=0; i--)
+        {
+            ans[i] *= right;
+            right *= nums[i];
+        }
+        return ans;
+    }
+};
