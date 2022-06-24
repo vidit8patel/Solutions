2f0f1f73fd92be1af70bd54294574ddcff4aae5b
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-06-24 17:03:42 +0400

    Time: 35 ms (87.42%), Space: 10.3 MB (81.96%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -38,3 +38,28 @@ public:
         return dp[k][n] = ans;
     }
 };
+
+// class Solution {
+// public:
+//     int dp[101][10001];
+//     int superEggDrop(int k, int n)
+//     {
+//         memset(dp, -1, sizeof(dp));
+//         return solve(k,n);
+//     }
+//     int solve(int k, int n) {
+//         if(n == 0 || n == 1)
+//             return n;
+//         if(k == 1)
+//             return n;
+//         if(dp[k][n] != -1)
+//             return dp[k][n];
+//         int ans = INT_MAX;
+//         for(int i = 1; i<=n; i++)
+//         {
+//             int temp = 1 + max(solve(k-1, i-1), solve(k, n-i));
+//             ans = min(ans, temp);
+//         }
+//         return dp[k][n] = ans;
+//     }
+// };
