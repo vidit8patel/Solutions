28c2cf515f85eac956d83c9f026c5336bf8bb0f8
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-06-24 17:03:14 +0400

    Time: 54 ms (84.53%), Space: 10.3 MB (73.68%) - LeetHub

---
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[101][10001];
+    int superEggDrop(int k, int n)
+    {
+        memset(dp, -1, sizeof(dp));
+        return solve(k,n);
+    }
+    int solve(int k, int n) {
+        if(n == 0 || n == 1)
+            return n;
+        if(k == 1)
+            return n;
+        if(dp[k][n] != -1)
+            return dp[k][n];
+        int ans = INT_MAX;        
+        int low = 1, high = n;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            int left = solve(k - 1, mid - 1);
+            int right = solve(k, n - mid);
+            ans = min(ans, 1+max(left, right));
+            if (left == right) 
+            {
+                break;
+            } 
+            else if (left < right) 
+            {
+                low = mid + 1;
+            } 
+            else 
+            {
+                high = mid;
+            }
+        }
+
+        return dp[k][n] = ans;
+    }
+};
