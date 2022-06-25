14cf3c84ab1cd58cee63184537ee22ff785fa525
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-06-25 12:07:23 +0400

    Time: 0 ms (100.00%), Space: 6 MB (70.07%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int ans = 0, factor = 2;
+        while(n > 1)
+        {
+            while(n % factor == 0)
+            {
+                ans += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        return ans;
+    }
+};
