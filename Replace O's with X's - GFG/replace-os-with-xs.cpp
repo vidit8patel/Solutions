0bd8509aeebe2a7c5466d7e525d42699c61b3a60
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-06-30 23:46:21 +0400

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    void dfs(vector<vector<char>> &mat,int i, int j, int n, int m)
+{
+   if(i<0 || j<0 || i>=n || j>=m || mat[i][j]!='O')
+   return;
+   mat[i][j]='B';
+   dfs(mat,i-1,j,n,m);
+   dfs(mat,i+1,j,n,m);
+   dfs(mat,i,j-1,n,m);
+   dfs(mat,i,j+1,n,m);
+}
+   vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+   {
+       // code here
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               if(i==0 || i==n-1 || j==0 || j==m-1)
+               dfs(mat,i,j,n,m);
+           }
+       }
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               if(mat[i][j]=='B')
+               mat[i][j]='O';
+               else if(mat[i][j]=='O')
+               mat[i][j]='X';
+           }
+       }
+       return mat;
+   }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}  // } Driver Code Ends
