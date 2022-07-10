a95602a3410aa77eb120b1b373a421fb8738aca9
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-07-10 12:17:20 +0400

    Time: 4 ms (100.00%), Space: 11.7 MB (100.00%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fillCups(vector<int>& A)
+    {
+        int mx = 0, sum = 0;
+        for(int& a: A)
+        {
+            mx = max(a, mx);
+            sum += a;
+        }
+        return max(mx, (sum + 1) / 2);
+    }
+};
