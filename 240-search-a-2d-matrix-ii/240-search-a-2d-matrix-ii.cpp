1ce67bc66db2063d3c8606bed6a1b6e507321d26
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-08-14 20:34:27 +0400

    Time: 133 ms (86.49%), Space: 14.9 MB (21.69%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,53 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        for(int i=0; i<m; i++){
-            int l=0, r=n-1;
-            while(l<=r){
-                int mid=(l+r)/2;
-                if(matrix[i][mid]==target)
-                    return true;
-                if(matrix[i][mid]<target)
-                    l=mid+1;
-                else
-                    r=mid-1;
-            }
+        int row = 0, col = matrix[0].size()-1;
+        while(row < matrix.size() && col >= 0)
+        {
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] > target)
+                col--;
+            else row++;
         }
         return false;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int row = 0, col = matrix[0].size()-1;
-//         while(row < matrix.size() && col >= 0)
-//         {
-//             if(matrix[row][col] == target)
-//                 return true;
-//             else if(matrix[row][col] > target)
-//                 col--;
-//             else row++;
-//         }
-//         return false;
-//     }
-// };
