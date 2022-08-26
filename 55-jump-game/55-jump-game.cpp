c5c8a1f565cd81eedbeccdc514716a1ea3f079d5
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-08-26 22:16:05 +0530

    Time: 104 ms (60.11%), Space: 48.5 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int dp[10001];
     bool canJump(vector<int>& nums) {
-        if(nums.size() == 1)
-            return true;
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, 0);
-    }
-    bool solve(vector<int>&nums, int i)
-    {
-        if(i >= nums.size()-1)
+        int n = nums.size();
+        int reachable = 0;
+        for(int i = 0; i<nums.size(); i++)
         {
-            return true;
+            if(i > reachable)
+                return false;
+            reachable = max(reachable, i + nums[i]);
         }
-        if(dp[i] != -1)
-            return dp[i];
-        int reach = i + nums[i];
-        for(int k = i+1; k<=reach; k++)
-        {
-            if(solve(nums, k))
-                return dp[i] = true;
-        }
-        return dp[i] = false;
+        return true;
     }
 };
+
+// class Solution {
+// public:
+//     bool canJump(vector<int>& A) {
+//         int i = 0;
+//         int reach = 0;
+//         for (; i < A.size() && i <= reach; ++i)
+//             reach = max(i + A[i], reach);
+//         return i == A.size();
+//     }
+// };
