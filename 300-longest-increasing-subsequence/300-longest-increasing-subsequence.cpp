af9393f7bac2dce113d0545852550d27fb1aca74
vidit8patel <105821053+vidit8patel@users.noreply.github.com>
2022-09-09 10:59:18 +0530

    Time: 14 ms (81.09%), Space: 10.4 MB (67.26%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,42 +1,43 @@
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1)
-            return 1;
-        vector<int> dp(nums.size(), 1);
-        int ans = INT_MIN;
-        for(int i = 1; i<n; i++)
-        {
-            for(int j = 0; j<i; j++)
-            {
-                if(nums[i] - nums[j] > 0)
-                {
-                    dp[i] = max(dp[i], 1+dp[j]);
-                }
-            }
-            ans = max(ans, dp[i]);
-        }
-        return ans;
-    }
-};
-
-
 // class Solution {
 // public:
 //     int lengthOfLIS(vector<int>& nums) {
-//         vector<int> arr;
-//         for(auto num : nums)
+//         int n = nums.size();
+//         if(n == 1)
+//             return 1;
+//         vector<int> dp(nums.size(), 1);
+//         int ans = INT_MIN;
+//         for(int i = 1; i < n; i++)
 //         {
-//             if(arr.size() == 0 || num > arr[arr.size()-1])
-//                 arr.push_back(num);
-//             else
+//             for(int j = 0; j < i; j++)
 //             {
-//                 auto it = lower_bound(arr.begin(), arr.end(), num);
-//                 int index = it - arr.begin();
-//                 arr[index] = num;
+//                 if(nums[i] - nums[j] > 0)
+//                 {
+//                     dp[i] = max(dp[i], 1+dp[j]);
+//                 }
 //             }
+//             ans = max(ans, dp[i]);
 //         }
-//         return arr.size();
+//         return ans;
 //     }
 // };
+
+class Solution
+{
+    public:
+        int lengthOfLIS(vector<int> &nums)
+        {
+            vector<int> arr;
+            for (auto num: nums)
+            {
+                if (arr.size() == 0 || num > arr[arr.size() - 1])
+                    arr.push_back(num);
+                else
+                {
+                    auto it = lower_bound(arr.begin(), arr.end(), num);
+                    int index = it - arr.begin();
+                    arr[index] = num;
+                }
+            }
+            return arr.size();
+        }
+};
